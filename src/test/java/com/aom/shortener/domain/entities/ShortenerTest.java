47792563204d2bb0f8e7d8b74c816237c52b49f0
Arley Oliveira <arley.msn@hotmail.com>
2022-12-30 08:47:05 -0300

    tests: adicionando testes unitários para a classe Shortener

---
+++ src/test/java/com/aom/shortener/domain/entities/ShortenerTest.java
@@ -0,0 +1,71 @@
+package com.aom.shortener.domain.entities;
+
+import com.aom.shortener.domain.exceptions.NotFound;
+import com.aom.shortener.domain.exceptions.ValidateException;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(SpringExtension.class)
+class ShortenerTest {
+
+    @Test
+    public void shouldCreateShortenerEntityWithSuccessful() {
+        //Arrange
+        var sourceUrlExpected = "www.test.com";
+        var shortenerExpected = "https://link.to/1234";
+
+        var result = new Shortener(sourceUrlExpected, shortenerExpected);
+
+        Assertions.assertThat(result.getSourceUrl()).isEqualTo(sourceUrlExpected);
+        Assertions.assertThat(result.getShortenedUrl()).isEqualTo(shortenerExpected);
+    }
+
+    @Test
+    public void shouldThrowExceptionOnCreationWhenShortenedUrlIsEmpty() {
+        //Arrange
+        var sourceUrlExpected = "www.test.com";
+        var shortenerExpected = "";
+
+        assertThrows(ValidateException.class, () -> {
+            new Shortener(sourceUrlExpected, shortenerExpected);
+        });
+    }
+
+    @Test
+    public void shouldThrowExceptionOnCreationWhenSourceUrlIsEmpty() {
+        //Arrange
+        var sourceUrlExpected = "";
+        var shortenerExpected = "https://link.to/1234";
+
+        assertThrows(ValidateException.class, () -> {
+            new Shortener(sourceUrlExpected, shortenerExpected);
+        });
+    }
+
+    @Test
+    public void shouldThrowExceptionOnCreationWhenShortenedUrlHasLengthLessThanFive() {
+        //Arrange
+        var sourceUrlExpected = "www.test.com";
+        var shortenerExpected = "http";
+
+        assertThrows(ValidateException.class, () -> {
+            new Shortener(sourceUrlExpected, shortenerExpected);
+        });
+    }
+
+    @Test
+    public void shouldThrowExceptionOnCreationWhenShortenedUrlHasLengthGreaterThanTwenty() {
+        //Arrange
+        var sourceUrlExpected = "www.test.com";
+        var shortenerExpected = "http://link.to/123456789abcdefgh";
+
+        assertThrows(ValidateException.class, () -> {
+            new Shortener(sourceUrlExpected, shortenerExpected);
+        });
+    }
+
+}
